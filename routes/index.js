bd2fb7420c67dd1fb309b3103cd3c7af46d1c488
Git 660 <achaldubey023@gmail.com>
2023-04-23 01:38:37 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,18 @@ router.get('/createfiles', function(req, res,) {
 });
 
 router.get('/createfolder',function(req,res){
-  fs.mkdir(`./uploads/${  req.query.foldername}`,0o777,function(err){
+  fs.mkdir(`./uploads/${  req.query.foldername}`,function(err){
     if(err) throw error;
     else{
       res.redirect("/")
     }
   })
+})
+router.get("/files/file/:data",function(req,res){
+  // fs.readdir(`./uploads/${data}`,function(err,data){
+  //   res.send(req.params['${data}'])
+  // })
+  res.send(req.params)
 })
 
 module.exports = router;
